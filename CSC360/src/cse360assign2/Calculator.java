12a9a486181717979cb9f1bcbe895b5c61423ed0
torre <etorre34@asu.edu>
2019-02-20 04:48:31 -0700

    Adding Calculator class, initial version.

---
+++ CSC360/src/cse360assign2/Calculator.java
@@ -0,0 +1,79 @@
+package cse360assign2;
+/**
+ * Calculator class, initial version, created by the instructor.
+ * <p>
+ * This class has methods and a constructor that will be used for another class that will act as a calculator.
+ * This class is the initial version the instructor created which all methods and variables must be added to new
+ * copies of this class two more times. This version of the Calculator class is just the empty methods provided
+ * and documented using the Javadoc style, keeping all methods blank.
+ * <p>
+ * 
+ * 
+ * @author Elias Torres
+ * Class ID: 565
+ * Assignment 2
+ *
+ */
+
+public class Calculator {
+	
+		/**
+		 * Constructor
+		 */
+		private int total;
+		
+		public Calculator () {
+			total = 0;  // not needed - included for clarity
+		}
+		
+		/**
+		 * Returns the total to zero
+		 * @return
+		 */
+		public int getTotal () {
+			return 0;
+		}
+		
+		/**
+		 * Mutator that adds the value parameter
+		 * @param value
+		 */
+		public void add (int value) {
+			
+		}
+		
+		/**
+		 * Mutator that subtracts the value parameter
+		 * @param value
+		 */
+		public void subtract (int value) {
+			
+		}
+		
+		/**
+		 * Mutator that multiplies the value parameter
+		 * @param value
+		 */
+		public void multiply (int value) {
+			
+		}
+		
+		/**
+		 * Mutator that divides the value parameter
+		 * @param value
+		 */
+		public void divide (int value) {
+			
+		}
+		
+		/**
+		 * A String method to return History
+		 * @return
+		 */
+		public String getHistory () {
+			return "";
+		}
+	}
+
+	
+	
